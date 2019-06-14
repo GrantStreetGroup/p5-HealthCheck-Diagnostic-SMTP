80aa4c2bbf7b3a8a37ff1a0621d0eed3d17a8b23
Mitch McCracken <mmccrack@grantstreet.com>
2019-06-14 10:57:05 -0400

    Initial working code

--- cpanfile
+++ cpanfile
@@ -2,6 +2,9 @@ use GSG::Gitc::CPANfile $_environment;
 
 # Add your requirements here
 
+requires 'HealthCheck::Diagnostic';
+requires 'Net::SMTP';
+
 on develop => sub {
     requires 'Dist::Zilla::PluginBundle::Author::GSG::Internal';
 };
--- lib/HealthCheck/Diagnostic/SMTP.pm
+++ lib/HealthCheck/Diagnostic/SMTP.pm
@@ -1,13 +1,100 @@
 package HealthCheck::Diagnostic::SMTP;
+
 use strict;
 use warnings;
 
-# ABSTRACT: Your Abstract Here
+use parent 'HealthCheck::Diagnostic';
+
+use Net::SMTP;
+
+# ABSTRACT: Verify connectivity to an SMTP mail server
 # VERSION
+
+sub new {
+    my ($class, @params) = @_;
+
+    my %params = @params == 1 && ( ref $params[0] || '' ) eq 'HASH'
+        ? %{ $params[0] } : @params;
+
+    return $class->SUPER::new(
+        id     => 'smtp',
+        label  => 'SMTP',
+        %params,
+    );
+}
+
+sub check {
+    my ($self, %params) = @_;
+
+    # Make it so that the diagnostic can be used as an instance or a
+    # class, and the `check` params get preference.
+    if ( ref $self ) {
+        $params{ $_ } = $self->{ $_ }
+            foreach grep { ! defined $params{ $_ } } keys %$self;
+    }
+
+    return $self->SUPER::check( %params );
+}
+
+sub run {
+    my ($self, %params) = @_;
+
+    my $server  = $params{server};
+    my $timeout = $params{timeout} // 5;
+
+    $server = $server->( %params ) if ref $server eq 'CODE';
+
+    local $@;
+    my $smtp = Net::SMTP->new( $server, Timeout => $timeout );
+
+    unless ( $smtp ) {
+        return {
+            status => 'CRITICAL',
+            info   => $@,
+        };
+    }
+
+    my $banner = $smtp->banner;
+    $smtp->quit;
+
+    return {
+        status => 'OK',
+        info   => $banner,
+    };
+}
 
 1;
 __END__
 
 =head1 SYNOPSIS
 
+Check that you can talk to the server.
+
+    my $health_check = HealthCheck->new( checks => [
+        HealthCheck::Diagnostic::SMTP->new(
+            server  => 'smtp.gmail.com',
+            timeout => 5,
+    ]);
+
 =head1 DESCRIPTION
+
+Determines if the SMTP mail server is available. Sets the C<status> to "OK" if
+the connection was successful, or "CRITICAL" otherwise.
+
+=head1 ATTRIBUTES
+
+Can be passed either to C<new> or C<check>.
+
+=head2 server
+
+Either a string of the hostname or a coderef that returns a hostname string.
+
+=head2 timeout
+
+The number of seconds to timeout after trying to establish a connection.
+Defaults to 5.
+
+=head1 DEPENDENCIES
+
+L<HealthCheck::Diagnostic>
+L<Net::SMTP>
